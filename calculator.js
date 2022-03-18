8a87543fbe084cadb6f00bec97c8b50853b037b9
Tzvi-H <16guitar@gmail.com>
2022-03-18 11:09:13 -0500

    added calculator functions

---
+++ calculator.js
@@ -0,0 +1,18 @@
+const calulator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+}
+
+function operate(operator, a, b) {
+  return calulator[operator](a, b);
+}
