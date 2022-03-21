d336b245b09b6c1d4599f048978f2e461a182421
Tzvi-H <16guitar@gmail.com>
2022-03-21 12:46:13 -0500

    added event listener for clicking on calculator
    and event handler skeleton

--- calculator.js
+++ calculator.js
@@ -16,3 +16,38 @@ const calulator = {
 function operate(operator, a, b) {
   return calulator[operator](a, b);
 }
+
+function addNumberToDisplay(number) {
+  console.log(number);
+}
+
+function displayCalculation(operation) {
+  console.log(`need to operate ${operation}`)
+}
+
+function clearDisplay() {
+  console.log('need to clear display');
+}
+
+function handleCalculatorClick(e) {
+  e.stopPropagation();
+  switch (true) {
+    case e.target.classList.contains('number'):
+      addNumberToDisplay(e.target.textContent);
+      break;
+    case e.target.classList.contains('operator'):
+      displayCalculation(e.target.textContent);
+      break;
+    case e.target.classList.contains('clear'):
+      clearDisplay()
+      break;
+    default:
+      console.log('clicked on something else');
+  }
+}
+
+function init() {
+  document.querySelector('table.calculator').addEventListener('click', handleCalculatorClick)
+}
+
+init()
